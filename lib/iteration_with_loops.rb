a3554fb15f2261a02daa3a6ae2ad1a91ad3f20f0
Mark De La Rosa <mark.p.dlr@gmail.com>
2020-04-05 22:29:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
   while row_i < src.count do
     col_i = 0
     while col_i < src[row_i].count do
-      if src[row_i][col_i].class ==  
+      if src[row_i][col_i].is_a?  ==  
       total = src[row_i][col_i].to_s
       col_i += 1
     end
